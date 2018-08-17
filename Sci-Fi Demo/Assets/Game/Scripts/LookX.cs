c3471c4e8b235a25913c27c9ace4da2047b09539
Juan Paixao <juanpaixao@live.com>
2018-08-16 22:16:54 -0300

    Added Player Movement

---
+++ Sci-Fi Demo/Assets/Game/Scripts/LookX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookX : MonoBehaviour
+{
+    [SerializeField]
+    private float _sensitivity = 1.3f;
+    void Update()
+    {
+        float _mouseX = Input.GetAxis("Mouse X");
+        Vector3 myRotation = transform.localEulerAngles;
+        myRotation.y += _mouseX*_sensitivity;
+        transform.localEulerAngles = myRotation;
+    }
+}
---
+++ Sci-Fi Demo/Assets/Game/Scripts/LookY.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookY : MonoBehaviour
+{
+    [SerializeField]
+    private float _sensitivity = 1.1f;
+    void Update()
+    {
+        float _mouseY = Input.GetAxis("Mouse Y");
+		Vector3 myRotation = transform.localEulerAngles;
+		myRotation.x -= Mathf.Clamp((_mouseY*_sensitivity),-80.0f,80.0f);
+		transform.localEulerAngles = myRotation;
+    }
+}
